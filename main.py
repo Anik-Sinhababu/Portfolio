a3fba63b135311d2f5b8bb40c906338befea7b71
ANIK <sinhababuanik@gmail.com>
2022-12-30 18:13:17 +0530

    added titles in columns of the webpage

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.image("new.png", width=400)
+    st.image("new.png", width=365)
 
 with col2:
     st.title("Anik Sinhababu")
-    content = """Hello!, I am Anik and I am a python programmer, and have a descent knowledge on django. 
-    I am pursuing my B-Tech Engineering in ECE department from the college Techno Main Saltlake,   
+    content = """Hello!, I am Anik and I am a python programmer, and have a descent knowledge on django. I am pursuing my B-Tech Engineering in ECE department from the college Techno Main Saltlake,   
     I have worked on several projects which are listed below. """
     st.info(content)
+st.subheader("Below are some of the fun apps developed by me, if you like any of these then feel free to contact me...")
 
-with col3:
-    st.title("Contact Info")
-    content = ("Email-Id:- sinhababuanik@gmail.com\nPhone-No:- 7478167687\nAddress:- Saltlake Ck-268 Bidhannagar\n"
-               "     Kolkata - 700091.")
-    st.info(content)
+col4, col5 = st.columns(2)
 
+df = pandas.read_csv("data.csv", sep=";")
+with col4:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col5:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
