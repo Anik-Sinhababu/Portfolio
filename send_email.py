1caa6382809cd4cfaebc0cbbe7bcc8bc480dccd7
ANIK <sinhababuanik@gmail.com>
2023-01-04 15:25:59 +0530

    all set and ready to deploy the webapp

--- send_email.py
+++ send_email.py
@@ -5,7 +5,7 @@ def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     username = "sinhababuanik@gmail.com"
-    password = "eesnkuelmoqivjsl"
+    password = "ikedmqaacwtbzdds"
     receiver = "sinhababuanik@gmail.com"
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=context) as server:
